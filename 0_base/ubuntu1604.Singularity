9001b2e6d643a172f7da135e088a5ddf885e4ed2
Tim H <thotfilter@osdr.org>
2018-12-18 10:10:39 +0000

    added images for gcc, python, datascience and a build script

---
+++ 0_base/ubuntu1604.Singularity
@@ -0,0 +1,17 @@
+Bootstrap: docker
+From: ubuntu:16.04
+
+%labels
+  Features base ubuntu 16.04, git, wget, curl
+
+%help
+  This is the base image for all cluster images
+
+%post
+  apt-get update
+  apt-get install -y vim
+  apt-get install -y git
+  apt-get install -y wget
+  apt-get install -y htop
+  apt-get install -y curl
+  apt-get install -y xz-utils
--- build-all.sh
+++ build-all.sh
@@ -3,17 +3,40 @@
 #build base scripts
 echo "## Building Base ##"
 
-mkdir -p output
+mkdir -p ./output
 
-#sudo singularity build ../output/base_ubuntu_1604.img ubuntu1604.Singularity
-#sudo singularity build ../output/base_ubuntu_1804.img ubuntu1804.Singularity
+cd ./0_base
+touch base_ubuntu_1604.log
+touch base_ubuntu_1804.log
+if [ ! -f ../output/base_ubuntu_1604.img ]; then
+  sudo singularity build ../output/base_ubuntu_1604.img ubuntu1604.Singularity > ./base_ubuntu_1604.log
+else
+  echo "    >> Not building base_ubuntu_1604, file exists"
+fi
+if [ ! -f ../output/base_ubuntu_1804.img ]; then
+  sudo singularity build ../output/base_ubuntu_1804.img ubuntu1804.Singularity > ./base_ubuntu_1804.log
+else
+  echo "    >> Not building base_ubuntu_1804, file exists"
+fi
+cd ..
 
 for dirs in * ; do
   if [ -d ${dirs} ]; then
-    if [[ ${dirs} != "base" ]] && [[ ${dirs} != ".git" ]] && [[ ${dirs} != "output" ]]; then
-      echo "## Building: " $dirs "##"
+    IMAGE=`cut -d'_' -f2,3 <<< "${dirs}"`
+
+    if [[ $IMAGE != "base" ]] && [[ $IMAGE != ".git" ]] && [[ $IMAGE != "output" ]]; then
+      echo "## Building: " ${IMAGE} "##"
       if [ -f ./${dirs}/Singularity ]; then
-        sudo singularity build ../output/${dirs}.img ./${dirs}/Singularity > ./${dirs}/build.log
+        #echo "hi"
+        OUTIMAGE=../output/${IMAGE}.img
+        if [ -f $OUTIMAGE ]; then
+          echo "    >> Not building ${IMAGE}, file exists"
+        else
+          cd ${dirs}
+          touch ./build.log
+          sudo singularity build OUTIMAGE ./Singularity > ./build.log
+          cd ..
+        fi
       fi
     fi
   fi
